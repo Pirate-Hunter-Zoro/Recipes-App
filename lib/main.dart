d2d6130f13350dfb49d483dbac0541c7d2bcfaae
Mikey Ferguson <90787330+Pirate-Hunter-Zoro@users.noreply.github.com>
2024-06-12 18:35:14 -0500

    included const modifier for Center Widget - until I continue the course

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: theme,
-      home: Center(),
+      home: const Center(),
     );
   }
 }
